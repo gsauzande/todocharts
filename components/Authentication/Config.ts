17d16a4585ca3e36ee1913ca005e85c022561f1a
Gásten Sauzande <gastensauzande@Gastens-MacBook-Pro.local>
2021-08-08 02:11:48 +0200

    Added env variable

--- components/Authentication/Config.ts
+++ components/Authentication/Config.ts
@@ -1,5 +1,5 @@
 export const config = {
   appId: "59a32171-71fc-40e0-88e9-99c7a84165d3",
-  redirectUri: "http://localhost:3000/dashboard",
+  redirectUri: process.env.REDIRECT_URI,
   scopes: ["user.read", "tasks.readwrite"],
 };
