962adcd99f1ae40fee884c417de94b3b1ea8537c
Gásten Sauzande <gastensauzande@Gastens-MacBook-Pro.local>
2021-08-09 19:18:59 +0200

    Add google analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -12,6 +12,15 @@ class MyDocument extends Document {
         <Head>
           <title>Todocharts</title>
           <link rel="shortcut icon" href="/static/favicon.ico" />
+          <!-- Global site tag (gtag.js) - Google Analytics -->
+          <script async src="https://www.googletagmanager.com/gtag/js?id=UA-126190436-3"></script>
+          <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'UA-126190436-3');
+          </script>
         </Head>
         <body>
           <Main />
